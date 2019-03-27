4a608b057952c2955f7c04206cbc4f82989ca02f
panli <n83@qq.com>
2019-03-27 17:37:26 +0800

    菜品名称显示长度7

--- TVAPPweb/src/app/AppConfig.ts
+++ TVAPPweb/src/app/AppConfig.ts
@@ -1,4 +1,4 @@
 export class AppConfig{
-    public static defaultIPAddr='192.168.99.28:5588';
+    public static defaultIPAddr='192.168.99.28:5599';
     public static APIUrlFormat='http://{url}/api/TVMealDisplay/?_action={action}';
 }
